c005604536d1ef95ad334d43f9f2fe8f4d976064
Vinello28 <74827522+Vinello28@users.noreply.github.com>
2024-03-29 18:30:11 +0100

    added ex1.2 (continue)

---
+++ Exercises/Exercice 1.2.py
@@ -0,0 +1,10 @@
+# TODO: Write a script that prints the name of the webpage with the best average response time among 6 websites.
+#  For the calculation
+#  of the average time, define the function average(list) that returns the average of the values contained in list.
+
+
+import requests
+
+websites = ['http://www.google.com', 'http://www.youtube.com', 'http://www.polimi.it', 'http://www.pornhub.com', 'http://www.univpm.it', 'http://www.facebook.com']
+
+#TODO da finire
