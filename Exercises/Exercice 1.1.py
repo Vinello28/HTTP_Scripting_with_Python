69f8b321d03d65a98dea14f9cea26803022cce24
Vinello28 <74827522+Vinello28@users.noreply.github.com>
2024-03-29 17:53:20 +0100

    added ex1.1

--- Exercises/Exercice 1.1.py
+++ Exercises/Exercice 1.1.py
@@ -1,4 +1,4 @@
-#TODO: write a python script to print the website name that have
+# TODO: write a python script to print the website name that have
 # the best response time. Use the following data: http://www.google.com and http://www.youtube.com
 
 import requests
@@ -8,9 +8,11 @@ url = ['http://www.google.com', 'http://www.youtube.com']
 r1 = requests.get(url[0])
 r2 = requests.get(url[1])
 
-if r1.elapsed.microseconds/1000 < r2.elapsed.microseconds/1000:
-    print(url[0],' wins with ', r1.elapsed.microseconds/1000)
-    print(url[1], ' lost with ', r2.elapsed.microseconds / 1000)
+response_time = [r1.elapsed.microseconds / 1000, r2.elapsed.microseconds / 1000]
+
+if response_time[0] < response_time[1]:
+    print(url[0], ' wins with ', response_time[0])
+    print(url[1], ' lost with ', response_time[1])
 else:
-    print(url[1], ' wins with ', r2.elapsed.microseconds/1000)
-    print(url[0], ' lost with ', r1.elapsed.microseconds / 1000)
+    print(url[1], ' wins with ', response_time[1])
+    print(url[0], ' lost with ', response_time[0])
